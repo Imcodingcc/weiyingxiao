cab80abd3969e48fe492b30adf292d9432016e5a
lvqiang <lvqiang@leither.org>
2018-03-21 10:18:13 +0800

    to null when the websocket is disconnected

--- app/src/main/java/com/leither/network/SendNotificationWebSocket.java
+++ app/src/main/java/com/leither/network/SendNotificationWebSocket.java
@@ -1,5 +1,6 @@
 package com.leither.network;
 
+import com.koushikdutta.async.callback.CompletedCallback;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.WebSocket;
 
@@ -25,12 +26,13 @@ public class SendNotificationWebSocket {
     }
 
     void onLanBoxIp(String ip) {
-        AsyncHttpClient.getDefaultInstance().websocket("ws://" + ip + ":8010/Message",
+        AsyncHttpClient.getDefaultInstance().websocket("ws://" + ip + ":8010/message",
                 null, (ex, webSocket) -> {
                     if (ex != null) {
                         ex.printStackTrace();
                         return;
                     }
+                    webSocket.setClosedCallback(ex1 -> this.webSocket = null);
                     this.webSocket = webSocket;
                 });
     }
